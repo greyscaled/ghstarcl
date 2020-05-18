8ec8ec412ee7d0c6c6a9e09b52dcb2331c07ec6c
G r e y <grey@coder.com>
2020-05-17 21:17:31 -0500

    A first (cruddy) impl

--- cmd/root.go
+++ cmd/root.go
@@ -23,28 +23,31 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
-)
+	"github.com/vapurrmaid/ghstarcl/service"
 
-var cfgFile string
+	"github.com/spf13/cobra"
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ghstarcl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A command line utility to manage Github stars.",
+	Run: func(cmd *cobra.Command, args []string) {
+		username, err := cmd.Flags().GetString("username")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		stars, err := service.GetRandGithubStars(username)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		for i := 0; i < len(stars); i++ {
+			star := stars[i]
+			fmt.Printf("\t* %s - %s\n", star.FullName, star.URL)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -57,41 +60,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ghstarcl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".ghstarcl" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".ghstarcl")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.Flags().StringP("username", "u", "", "Github username")
+	rootCmd.MarkFlagRequired("username")
 }
---
+++ service/stars.go
@@ -0,0 +1,79 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type StarredRepo struct {
+	ID          int    `json:"id"`
+	FullName    string `json:"full_name"`
+	URL         string `json:"html_url"`
+	Description string `json:"description"`
+}
+
+// GetRandGithubStars returns a random page of repositories that the provided
+// username has starred on Github.
+func GetRandGithubStars(username string) ([]StarredRepo, error) {
+	res, err := http.Head(makeGithubURL(username))
+	if err != nil {
+		return nil, err
+	}
+
+	nPages, err := getNumberOfPages(res)
+	if err != nil {
+		return nil, err
+	}
+
+	var stars []StarredRepo
+	var url string
+	if nPages == 1 {
+		url = makePagedGithubURL(username, 1)
+	} else {
+		url = makePagedGithubURL(username, rand.Intn(nPages-1)+1)
+	}
+
+	res, err = http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	err = json.Unmarshal(body, &stars)
+	if err != nil {
+		return nil, err
+	}
+
+	return stars, nil
+}
+
+func makeGithubURL(username string) string {
+	return strings.Join([]string{"https://api.github.com/users", username, "starred"}, "/")
+}
+
+func makePagedGithubURL(username string, page int) string {
+	return fmt.Sprintf("%s%s%d", makeGithubURL(username), "?page=", page)
+}
+
+func getNumberOfPages(res *http.Response) (int, error) {
+	links := strings.Split(res.Header.Get("link"), ",")
+	lastLink := links[len(links)-1]
+	re := regexp.MustCompile(`\?page=(\d+)>`)
+	matches := re.FindStringSubmatch(lastLink)
+	pagestr := matches[len(matches)-1]
+	pageint, err := strconv.ParseInt(pagestr, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int(pageint), nil
+}
